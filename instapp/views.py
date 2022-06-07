69033f32ae1f5a7cb11089aed8411d6a3abf40ad
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-07 23:08:14 +0300

    created a view function for authentication

--- instapp/views.py
+++ instapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+# registration view function
+
+def register(request):
+    return render (request, 'insta/register.html')
+
 def home(request):
-    return HttpResponse('hello i am working')
+    return render (request, 'insta/index.html')
+
