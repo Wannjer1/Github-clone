c2f35ca633a451fcc88cebb71f5285ba9e2e976e
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 02:23:33 +0300

    st-styling modofications for login page

--- instapp/urls.py
+++ instapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('',views.login, name='login'),
