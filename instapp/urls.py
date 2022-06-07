d1efc32d0a21ec342bb3ac51a10daa48edf58575
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 02:55:12 +0300

    updated logout path

--- instapp/urls.py
+++ instapp/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
     path('',views.login, name='login'),
     path('register/',views.register, name='register'),
     path('home/',views.home, name='home'),
-    path('logout/',views.logout, name='logout'),
+    path('logout/',auth_view.LogoutView.as_view(template_name='insta/logout.html'), name="logout"),
 ]
 
 # if settings.DEBUG:
