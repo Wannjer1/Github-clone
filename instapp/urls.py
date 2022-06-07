54a7da3da4e6791f2f15433952577c077633c160
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 01:17:34 +0300

    created path to the registration template

--- instapp/urls.py
+++ instapp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',views.register, name='register'),
+    path('',views.login, name='login'),
+    path('register/',views.register, name='register'),
     path('home/',views.home, name='home'),
 ]
 
