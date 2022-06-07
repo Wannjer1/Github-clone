5df24d8b521cb558e37b9bf63859ef1db8db680f
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-07 22:11:25 +0300

    added a route to the home page

--- instapp/urls.py
+++ instapp/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path 
+from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    # path('',indexpage, name='index')
+    path('',views.home, name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
