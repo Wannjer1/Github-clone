0e42d00a4a97f9c0c6681afec6abb0f105523b0b
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-07 22:12:04 +0300

    created function to test route

--- instapp/views.py
+++ instapp/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('hello i am working')
