1e4291a2535e1d7b021b5617d89bc67950f14d10
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 03:26:11 +0300

    created profiles model

--- instapp/models.py
+++ instapp/models.py
@@ -1,4 +1,4 @@
-from tkinter import image_names
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -8,7 +8,7 @@ from django.dispatch import receiver
 class Image(models.Model):
     '''Model to display image posts'''
     image = models.ImageField(upload_to='images/')
-    image_name = models.CharField()
+    name = models.CharField(max_length=255, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     caption = models.TextField()
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
@@ -17,4 +17,5 @@ class Image(models.Model):
 class Profile(models.Model):
     '''model to display users profile'''
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+    photo = models.ImageField(default='default.jpg', upload_to='avatars/')
     bio = models.TextField(max_length=500,blank=True,null=True, default=f'Hello, I am new here!')
