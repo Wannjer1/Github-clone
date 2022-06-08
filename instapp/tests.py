5e25d543cb5dc66a0a5f84f312e17eb4a1739ef7
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 05:11:58 +0300

    image model test case

--- instapp/tests.py
+++ instapp/tests.py
@@ -19,3 +19,29 @@ class ProfileTest(TestCase):
         self.assertIsInstance(self.user.profile, Profile)
         self.user.save()
         self.assertIsInstance(self.user.profile, Profile)
+
+# image model tests
+class TestImage(TestCase):
+    ''' test class for image model '''
+    def setUp(self):
+        ''' method called before each test case'''
+        self.test_user = User(username='Linda', password='123')
+        self.test_user.save()
+        self.test_profile = self.test_user.profile
+        self.test_profile.save()
+
+        self.test_image = Image(image='images/test.jpg', caption='some text', profile=self.test_profile, created_on=datetime.now())
+
+    def test_instance(self):
+        ''' test method to ensure image instance creation '''
+        self.assertTrue(isinstance(self.test_image, Image))
+
+    def test_save(self):
+        ''' test method to save image instance to db '''
+        self.test_image.save_image()
+        self.assertEqual(len(Image.objects.all()), 1)
+
+    def tearDown(self):
+        ''' method to clear all setup instances after each test run '''
+        self.test_user.delete() #deletes it's profile too
+        Image.objects.all().delete()
