b4cf7cce226854aa53676e0725c6d3af38b00592
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 03:15:39 +0300

    created images model

--- instapp/models.py
+++ instapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Image(models.Model):
+    '''Model to display image posts'''
+    image = models.ImageField(upload_to='images/')
+    created_on = models.DateTimeField(auto_now_add=True)
+    caption = models.TextField()
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
