5ad3cd76199c3fd87ae478b1c29a2d77c40c7e80
Ann Wanjeri <wannjeri2@gmail.com>
2022-06-08 04:43:56 +0300

    testcase for the profile model created

--- instapp/tests.py
+++ instapp/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Image, Profile
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTest(TestCase):
+    ''' test class for Profile model'''
+    def setUp(self):
+        ''' method called before each test case'''
+        self.user = User.objects.create_user(username='Water')
+
+    def tearDown(self):
+        ''' method to clear all setup instances after each test run '''
+        self.user.delete()
+
+    def test_profile_creation(self):
+        ''' method to test profile instance is created only once for each user '''
+        self.assertIsInstance(self.user.profile, Profile)
+        self.user.save()
+        self.assertIsInstance(self.user.profile, Profile)
